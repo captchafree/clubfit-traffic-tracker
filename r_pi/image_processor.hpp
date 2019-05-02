4a426b5025959ca40c301007c0cf69e988039665
Josh Beck <beckj3@rpi.edu>
2019-05-02 15:50:42 -0400

    Refactored

---
+++ r_pi/image_processor.hpp
@@ -0,0 +1,114 @@
+#include <iterator>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/video.hpp>
+
+#include "config.hpp"
+
+class ImageProcessor {
+    public:
+        ImageProcessor() {
+            this->fgbg = cv::createBackgroundSubtractorMOG2();
+
+            Config config;
+            this->machines = config.get_machines();
+
+            // std::ifstream file("config.csv");
+            // CSVRow row;
+            // while(file >> row) {
+            //     std::vector<cv::Point> corners;
+            //     for(int i = 0; i < row.size(); i+=2) {
+            //         corners.push_back(cv::Point(stoi(row[i]), stoi(row[i+1])));
+            //     }
+            //     this->detection_locations.push_back(corners);
+            // }
+        }
+
+        /*
+        Process the frame and return the result to show
+        */
+        cv::Mat3b process(cv::Mat const &frame) {
+            //Show original footage
+            //cv::imshow("Original", frame);
+            
+            //Apply frame to background subtractor
+            this->fgbg->apply(frame, this->fgMask);
+
+            cv::Mat3b result;
+            //Convert single-channel image to BGR
+            cv::cvtColor(this->fgMask, result, cv::COLOR_GRAY2BGR);
+
+            //Apply binary threshold
+            cv::threshold(result, result, 250, 255, 0);
+            cv::erode(result, result, cv::Mat(), cv::Point(-1, -1), 2, 1, 1);
+            cv::dilate(result, result, cv::Mat(), cv::Point(-1, -1), 3, 1, 1);
+
+            int index = 1;
+            //Iterate over each detection location
+            for(std::vector<Machine>::iterator it = this->machines.begin(); it != this->machines.end(); ++it) {
+                
+                std::string name = it->name;
+                std::vector<cv::Point> points = it->detection_locations;
+
+
+                //Generate mask
+                cv::Mat mask(result.size().height, result.size().width, CV_8UC3, cv::Scalar(0, 0, 0));
+
+                int num_points = points.size();
+                int num_polygons = 1;
+                int line_type = 8;
+                cv::Point corners[1][num_points];
+                for(int i = 0; i < num_points; i++) {
+                    corners[0][i] = (points)[i];
+                }
+                const cv::Point* corner_list[1] = { corners[0] };
+                cv::fillPoly(mask, corner_list, &num_points, num_polygons, cv::Scalar(255, 255, 255), line_type);
+
+                cv::Mat cropped;
+                cv::bitwise_and(result, mask, cropped);
+
+                //Get bounding box
+                cv::Rect bounding_box = cv::boundingRect(points);
+                cropped = cropped(bounding_box);
+                
+                //Invert image
+                cv::Mat cropped_grayscale;
+                cv::cvtColor(cropped, cropped_grayscale, cv::COLOR_BGR2GRAY);
+                int white = cv::countNonZero(cropped_grayscale);
+
+                //Calculate percent of image that is white pixels
+                int total = cv::contourArea(points);
+                std::vector<cv::Mat> channels;
+                cv::split(cropped, channels);
+                float avg = float(white) / float(total) * 100;
+                std::vector<std::vector<cv::Point>> contours;
+                contours.push_back(points);
+                
+                if(avg > 35) {
+                    //std::cout << "Object Detected in box " << index << " (% white = " << avg << ")" << std::endl;
+                    std::cout << "Machine `" << name << "` is in use" << std::endl;
+                    cv::drawContours(result, contours, 0, cv::Scalar(0,255,0), 1, 8);
+                } else {
+                    cv::drawContours(result, contours, 0, cv::Scalar(255,255,255), 1, 8);
+                }
+
+                index++;
+            }
+
+            return result;
+        }
+
+    private:
+        cv::Ptr<cv::BackgroundSubtractor> fgbg;
+        cv::Mat fgMask;
+
+        std::vector<Machine> machines;
+};
---
+++ r_pi/machine.hpp
@@ -0,0 +1,13 @@
+#include <iterator>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+
+#include <opencv2/imgcodecs.hpp>
+
+struct Machine {
+    std::string name;
+    std::vector<cv::Point> detection_locations;
+};
